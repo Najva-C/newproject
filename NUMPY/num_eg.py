c17033f4bceda428ee0f48b88ce08234c5c56a2f
Najva-C <najvajuva123@gmail.com>
2025-08-23 11:47:22 +0530

    python program (CC)

--- NUMPY/num_eg.py
+++ NUMPY/num_eg.py
@@ -2,8 +2,40 @@ import numpy as np
 # a = np.array([1,2,3])
 # print(a)
 
-print(np.zeros((2,3)))
-print(np.ones((2,2)))
+# print(np.zeros((2,3)))
+# print(np.ones((2,2)))
 
-a=np.arange(0,10,2)
-print(a)
+# a=np.arange(0,10,2)
+# print(a)
+
+# a=np.arange(6)
+# b = a.reshape((2,3))
+# print("Reshaped:\n" ,b)
+
+# arr = np.array([1,2,3,4])
+# print(np.mean(arr))
+# print(np.median(arr))
+# print(np.std(arr))
+
+
+# RANDOM NUMBERS OF FLOAT
+# a = np.random.rand()
+# print(a)
+# b = np.random.rand(5)
+# print(b)
+# c = np.random.rand(2,3)
+# print(c)
+
+
+# RANDOM NUMBERS OF INTEGER
+# x = np.random.randint(1,10)
+# print(x)
+# y = np.random.randint(0,100,size=5)
+# print(y)
+# z = np.random.randint(10,21 ,size=(3,3))
+# print(z)
+
+# OTP EXAMPLE
+otp = np.random.randint(100000,1000000)
+print(otp)
+
