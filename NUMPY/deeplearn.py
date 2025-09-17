66d7dfa94041b5d6766f054d6de5a5f564f5e56d
Najva-C <najvajuva123@gmail.com>
2025-09-17 13:41:39 +0530

    python program(CC)

--- NUMPY/deeplearn.py
+++ NUMPY/deeplearn.py
@@ -10,3 +10,7 @@ import matpotlib.pyplot as plt
 # NORMALISE DATA (SCALE PIXEL VALUES TO 0.1)
 x_train = x_train.astype("float32") / 255.0
 x_test = x_test.astype("float32")  /255.0
+
+# RESHAPE DATA(CNN EXPECTS 3D:HEIGHT,WIDTH,CHANNELS)
+x_train = x_train.reshape(-1,28,28,1)
+x_test = x_test.reshape(-1,28,28,1)
